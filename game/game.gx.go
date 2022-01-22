865da530465cc2bb1db4824ece28572f8c8bf9b1
Nikhilesh S <s.nikhilesh@gmail.com>
2022-01-21 16:16:02 -0800

    quick entities demo

--- game/game.gx.go
+++ game/game.gx.go
@@ -3,6 +3,7 @@
 package game
 
 import (
+	. "github.com/nikki93/dream-hotel/core/entity"
 	. "github.com/nikki93/dream-hotel/core/geom"
 	"github.com/nikki93/dream-hotel/core/rl"
 )
@@ -15,11 +16,24 @@ var gameCamera = rl.Camera2D{
 var gameTime = 0.0
 var deltaTime = 0.0
 
+type Circle struct {
+	Behavior
+
+	Pos    Vec2
+	Radius float64
+}
+
 //
 // Init
 //
 
 func initGame() {
+	CreateEntity(
+		Circle{
+			Pos:    Vec2{100, 200},
+			Radius: 20,
+		},
+	)
 }
 
 //
@@ -29,6 +43,10 @@ func initGame() {
 func updateGame(dt float64) {
 	gameTime += dt
 	deltaTime = dt
+
+	Each(func(ent Entity, circle *Circle) {
+		circle.Pos.Y += 80 * Sin(3*rl.GetTime()) * dt
+	})
 }
 
 //
@@ -36,4 +54,9 @@ func updateGame(dt float64) {
 //
 
 func drawGame() {
+	Each(func(ent Entity, circle *Circle) {
+		rl.DrawCircleV(circle.Pos, circle.Radius, rl.Red)
+	})
+
+	rl.DrawRectangleLines(0, 0, 864, 486, rl.Red)
 }
